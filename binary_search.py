64964c30a331fe44c1f1b623493c2bcb0a6688a6
Fernando Rengifo <ferengifo@MacBook-Air-de-Fernando.local>
2020-04-27 23:55:25 -0500

    Ajustes ejercicio busqueda binaria

--- binary_search.py
+++ binary_search.py
@@ -18,7 +18,7 @@ def binary_search(numbers, number_to_find, low, high):
 if __name__ == "__main__":
     numbers = [1, 3, 4, 5, 6, 9, 10, 11, 25, 27, 28, 34, 36, 49, 51]
 
-    number_to_find = int(raw_input('Ingresa un número: '))
+    number_to_find = int(input('Ingresa un número: '))
 
     result = binary_search(numbers, number_to_find, 0, len(numbers) - 1)
 
